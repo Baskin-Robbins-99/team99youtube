c887ee7d03e25e023d2cf6adc625d07bb4adce2f
phr08 <thwn1012@gmail.com>
2023-09-25 16:46:36 +0900

    first commit

--- app/src/main/java/com/example/team99/MainActivity.kt
+++ app/src/main/java/com/example/team99/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.team99
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.team99.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navigationItemSelect()
+    }
+
+    private fun navigationItemSelect(){
+        binding.mainNav.run {
+            setOnItemReselectedListener {
+                    item ->
+                when(item.itemId){
+                    R.id.item_home -> replaceFragment(HomeFragment())
+                    R.id.item_my_video -> replaceFragment(MyVideoFragment())
+                    R.id.item_search ->replaceFragment(SearchFragment())
+                }
+                true
+            }
+            selectedItemId = R.id.home_frag
+        }
+        replaceFragment(fragment = HomeFragment())
+    }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_frame, fragment)
+        fragmentTransaction.commit()
     }
 }
---
+++ app/src/main/java/com/example/team99/VideoDetailActivity.kt
@@ -0,0 +1,25 @@
+package com.example.team99
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.team99.databinding.ActivityVideoDetailBinding
+
+class VideoDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVideoDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityVideoDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        back()
+
+    }
+    private fun back() = with(binding) {
+        vidieoBack.setOnClickListener {
+            finish()
+        }
+    }
+
+}
