106c297346954621bef6bd0a6f24281b1eea5fc7
asdsad86642 <asdsad86642@gmail.com>
2023-10-04 20:13:57 +0900

    [FEAT] 8:13분 코드공유

--- app/src/main/java/com/example/team99/Search/SearchVideoViewModel.kt
+++ app/src/main/java/com/example/team99/Search/SearchVideoViewModel.kt
@@ -6,13 +6,46 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class SearchVideoViewModel : ViewModel() {
     val searchResult: MutableLiveData<List<SearchVideoItem>> = MutableLiveData()
     val isLoading: MutableLiveData<Boolean> = MutableLiveData()
     val errorMessage: MutableLiveData<String> = MutableLiveData()
 
+
     fun searchVideos(query: String) {
-        // [API call and UI update logic]
+        // Check if the query is not blank
+        if(query.isBlank()) {
+            errorMessage.value = "Query must not be blank"
+            return
+        }
+
+        // Indicate that loading has started
+        isLoading.value = true
+
+        viewModelScope.launch {
+            try {
+                val response = RetrofitInstance.api.searchVideos(
+                    part = "snippet",
+                    query = query,
+                    key = "AIzaSyCvE-aHt4WSh1CqDE1aIzzLRLByxBgR9GI"
+                )
+                handleResponse(response)
+            } catch (e: Exception) {
+                errorMessage.value = "An error occurred: ${e.localizedMessage}"
+            } finally {
+                // Indicate that loading has finished, whether successful or not
+                isLoading.value = false
+            }
+        }
+    }
+
+    private fun handleResponse(response: Response<VideoResponse>) {
+        if(response.isSuccessful) {
+            searchResult.value = response.body()?.items
+        } else {
+            errorMessage.value = "Failed to load data: ${response.message()}"
+        }
     }
 }
