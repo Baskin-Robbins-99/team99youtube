5e278cc799a206d7933f97b42e125bacafe1531d
phr08 <thwn1012@gmail.com>
2023-10-04 17:25:12 +0900

    [FEAT]  데이터 sharedPreferences로 보내기

--- app/src/main/java/com/example/team99/MyVideoFragment/View/MyVideoFragment.kt
+++ app/src/main/java/com/example/team99/MyVideoFragment/View/MyVideoFragment.kt
@@ -2,47 +2,31 @@ package com.example.team99.MyVideoFragment.View
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.room.Room
-import androidx.room.RoomOpenHelper
-import com.example.team99.MyVideoFragment.Database.AppDatabase
-import com.example.team99.MyVideoFragment.Database.StorageDAO
-import com.example.team99.MyVideoFragment.Database.StorageDatabase
-import com.example.team99.MyVideoFragment.DummyData
+import com.example.team99.Home.VideoItem
+import com.example.team99.MainActivity
 import com.example.team99.MyVideoFragment.MyStorageAdapter
 import com.example.team99.databinding.FragmentMyVideoBinding
-
 
 class MyVideoFragment : Fragment() {
     private lateinit var binding: FragmentMyVideoBinding
     lateinit var mystorageadapter: MyStorageAdapter
-    lateinit var helper: RoomOpenHelper
-    lateinit var storageDao: StorageDAO
-    private lateinit var myvideos : Context
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        myvideos = requireContext()
-    }
+    private val myvideo = mutableListOf<VideoItem>()
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         binding = FragmentMyVideoBinding.inflate(inflater, container, false)
-        saveStorage()
-
+        mystorageadapter = MyStorageAdapter(requireContext(),myvideo)
+        binding.myvdRcStorage.adapter = mystorageadapter
         return binding.root
-    }
-
-
-    private fun saveStorage()  {
-        mystorageadapter = MyStorageAdapter(myvideos)
-            binding.myvdRcStorage.adapter = mystorageadapter
     }
 }
 
