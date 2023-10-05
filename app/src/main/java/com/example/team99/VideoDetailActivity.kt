41b1ad622be3fe41aff22241796a942e6434a34d
yoonhee~ <flvps123@gmail.com>
2023-10-05 18:39:16 +0900

    [DESIGN] : 🎨 splash 추가

--- app/src/main/java/com/example/team99/VideoDetailActivity.kt
+++ app/src/main/java/com/example/team99/VideoDetailActivity.kt
@@ -1,31 +1,22 @@
 package com.example.team99
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.webkit.WebSettings
 import android.webkit.WebView
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.example.team99.Home.HomeFragment
 import com.example.team99.Home.VideoAdpter
-import com.example.team99.Home.VideoItem
-import com.example.team99.Retrofit.RetrofitClient
 import com.example.team99.databinding.ActivityVideoDetailBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 
 
 class VideoDetailActivity : AppCompatActivity() {
 
     private var isFavorite = false
-    private lateinit var webView: WebView
     private lateinit var mContext: Context
     private lateinit var adapter: VideoAdpter
     private lateinit var binding: ActivityVideoDetailBinding
@@ -39,13 +30,7 @@ class VideoDetailActivity : AppCompatActivity() {
 
         binding = ActivityVideoDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    //    webView = findViewById(R.id.web_view)
 
-       // val webSettings: WebSettings = webView.settings
-      //  webSettings.javaScriptEnabled = true
-
-        //val videoId = "C3GouGa0noM"
-       // val iframeCode = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/$videoId\" frameborder=\"0\" allowfullscreen></iframe>"
         val videotitle = intent.getStringExtra("title")
         val videodescription = intent.getStringExtra("description")
         val thumbnailUrl = intent.getStringExtra("thumbnailUrl")
@@ -56,16 +41,13 @@ class VideoDetailActivity : AppCompatActivity() {
             .load(thumbnailUrl) // 이미지 URL
             .into(binding.imageView99)
 
-        // WebView에 iframe 코드 로드
-     //   webView.loadData(iframeCode, "text/html", "utf-8")
-        Log.d("VideoDetailActivity", "Iframe 코드가 WebView에 로드되었습니다.")
 
-        // 이미지 버튼 클릭 시
+
        binding.btnBack.setOnClickListener {
            finish()
         }
 
-        // 즐겨찾기 이미지 클릭 시
+
         findViewById<ImageView>(R.id.iv_detailLike).setOnClickListener {
             if (!isFavorite) {
                 isFavorite = true
