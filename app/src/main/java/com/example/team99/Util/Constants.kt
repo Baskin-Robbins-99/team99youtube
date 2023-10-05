284f8d3d7308583e0fb9e7d34133548dff41f1f9
kwonyongil <kkwonyi95@gmail.com>
2023-10-05 14:37:09 +0900

    [FIX] 키값 할달량 오버

--- app/src/main/java/com/example/team99/Util/Constants.kt
+++ app/src/main/java/com/example/team99/Util/Constants.kt
@@ -4,7 +4,9 @@ object Constants {
 
     var BASE_URL = "https://youtube.googleapis.com/"
 
-    var API_KEY = ">AIzaSyBx5x3nhrglEpE6nZqj37ywin9WJW9WhDc"
+    var API_KEY = "AIzaSyBx5x3nhrglEpE6nZqj37ywin9WJW9WhDc"
 }
-
-//
+// 권용일 - AIzaSyBx5x3nhrglEpE6nZqj37ywin9WJW9WhDc
+// 이성진 - AIzaSyA-_9u-D9v7zgza9kxOSrw3DntYcgTRaq8
+// 남윤희 - AIzaSyB6LQTuxxO_UqyPtss6vgZEDYWH1NZk0Cs
+// 박해린 - AIzaSyDQcuG4rMQa3TFfP0Wf4oF0TRe05vv7cvk
